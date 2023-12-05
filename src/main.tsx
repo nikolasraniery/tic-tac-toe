67597927571dda9d70ded74403c8c0a2c87468c0
nikolasraniery <nikolassraniery@gmail.com>
2023-12-05 15:06:39 -0300

    Jogo-da-velha(tic-tac-toe)

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
