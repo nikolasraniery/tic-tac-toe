6faa1f3f9987017593da977e629dfd8ea1c79d5f
nikolasraniery <nikolassraniery@gmail.com>
2023-12-05 15:18:45 -0300

    "retirando imports"

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import './index.css'
